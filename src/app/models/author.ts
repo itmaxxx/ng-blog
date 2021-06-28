8eb9746ace8b305fa99b3ae3fe5e5a2f991a1d67
itmaxxx <dmitrievmaks77@gmail.com>
2021-06-28 19:55:10 +0300

    fonts and models update

---
+++ src/app/models/author.ts
@@ -0,0 +1,11 @@
+export class Author {
+  username: string;
+  fullname: string;
+  picture: string;
+
+  constructor() {
+    this.username = 'null';
+    this.fullname = 'null';
+    this.picture = 'null';
+  }
+}
--- src/app/models/post.ts
+++ src/app/models/post.ts
@@ -1,11 +1,17 @@
+import { Author } from "./author";
+
 export class Post {
   id: number;
   title: string;
   text: string;
+  picture: string;
+  author: Author;
 
   constructor() {
-    this.id = 1;
+    this.id = -1;
     this.title = "null";
     this.text = "null";
+    this.picture = "null";
+    this.author = new Author();
   }
 }
