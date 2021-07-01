cb41db23f433575f53126aa0b6af8ebcb851f5a9
itmaxxx <dmitrievmaks77@gmail.com>
2021-07-01 10:47:27 +0300

    search query

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -12,6 +12,32 @@ export class NavBarComponent implements OnInit {
 
   constructor() { }
 
+  handleSubmit(e: any): void {
+    e.preventDefault();
+
+    this.search();
+  }
+
+  search(): void {
+    const searchQuery = document.getElementById('search-query') as HTMLInputElement;
+    const query = searchQuery.value.trim();
+
+    if (query.length > 0) {
+      console.log(query);
+      // TODO: get articels by query
+
+      // this.articleService.getArticles().subscribe(articles => {
+      //   this.articles = articles.filter(a => {
+      //     return a.title.toLowerCase().includes(text.toLowerCase());
+      //   })
+      // })
+    }else {
+      // TODO: get all articles
+
+      // this.articleService.getArticles().subscribe(articles => this.articles = articles);
+    }
+  }
+
   ngOnInit(): void {
   }
 
