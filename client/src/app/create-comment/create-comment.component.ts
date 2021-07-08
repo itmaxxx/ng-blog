0e628c1e7f8f5a978a6ce5012a4e45203e74c03b
itmaxxx <dmitrievmaks77@gmail.com>
2021-07-08 14:26:13 +0300

    create comment component

---
+++ client/src/app/create-comment/create-comment.component.ts
@@ -0,0 +1,48 @@
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {CommentsService} from "../services/comments.service";
+import {Comment} from "../models/comment";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-create-comment',
+  templateUrl: './create-comment.component.html',
+  styleUrls: ['./create-comment.component.css']
+})
+export class CreateCommentComponent implements OnInit {
+
+  @Input() postID: number = -1;
+
+  constructor(private commentsService: CommentsService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  validate(comment: Comment) {
+    const minLength = 4;
+
+    if (comment.text.length < minLength) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  createComment(e: any) {
+    e.preventDefault();
+
+    const text = (document.getElementById('commentText') as HTMLInputElement).value.trim();
+    const authorUsername = (document.getElementById('commentAuthorUsername') as HTMLInputElement).value.trim();
+    const comment = new Comment(text, this.postID, authorUsername);
+
+    if (!this.validate(comment)) {
+      alert('Some of the fields are empty or less than 3 symbols');
+
+      return;
+    }
+
+    this.commentsService.createComment(comment).subscribe(comment => {
+      console.log(comment);
+    });
+  }
+
+}
